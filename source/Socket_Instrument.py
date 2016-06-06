ed145f3654ad00a3f534354c5938aaf2311f6e48
sandeshkalantre <kalantresandesh@gmail.com>
2016-06-06 17:37:29 +0200

    AWG initialized through visa.py instead of Socket_instrument

--- source/Socket_Instrument.py
+++ source/Socket_Instrument.py
@@ -15,27 +15,32 @@ import time
 
 class Socket_Instrument(object):
     ''' socket replacement for visa.instrument class'''
-    def __init__(self, IPaddress, PortNumber = 4000):
+    def __init__(self, IPaddress, PortNumber = 4000, timeout = 0.2):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((IPaddress, PortNumber))
-        self.s.setblocking(True)
+        self.s.setblocking(0)
+        self.s.settimeout(timeout)
 
     def write(self, cmd):
         self.s.send(cmd + '\n')
 
     def ask(self, cmd, buffer = 1024, timeout = 5):
         self.s.send(cmd + '\n')
-        response = ''
+        response = ''   
         while True:
+
             char = ""
             try:
-                char = self.s.recv(1)
+                char = self.s.recv(24)
+                if char == '':
+                    break
             except:
                 time.sleep(0.1)
                 if response.rstrip() != "":
                     break
             if char:
                 response += char
+
         return response.rstrip()
 
     def read(self):
