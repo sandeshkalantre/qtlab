01fc650db71d4d9cbfd310bac56f32c61af5497e
nanoelectronics-new <josip.kukucka@ist.ac.at>
2016-07-17 20:56:33 +0200

    Sequence generation working properly, AWG_lib cleaned and commented

--- MeasureScripts/AWG_lib.py
+++ MeasureScripts/AWG_lib.py
@@ -24,7 +24,23 @@ AWG = qt.instruments.get("AWG")
 
 
 
-def set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync = None, compensate = None, t_sync = 2):
+def set_waveform(seq,AWG_clock,AWGMax_amp, sync = None, compensate = None, t_sync = 2):
+    '''
+    This function uploads and loads previously created sequence to the AWG. 
+       
+        
+        Input:
+            seq (list) : list of sequences for every channel
+            AWG_clock (int) : AWG clock
+            AWGMax_amp : # In Volts!!! Maximum needed amplitude on all channels for your particular experiment (noise reduction) 
+            sync (Waveform object) : Synchronization element of sequence
+            t_sync (float) = Duration of synchronization element of sequence, in TimeUnits
+            compensate (Waveform object) : Compensate (for mean value) element of sequence
+                      
+            
+        Output:
+            None
+        '''
     
    
     if (sync == None) or (compensate == None):
@@ -40,33 +56,8 @@ def set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync = None, compensate = 
     AWG.set_clock(AWG_clock)  # Set AWG clock
 
 
-    ## Making HP inverse on CH1      # ADDED
-    #CompleteSeqWav = np.array([])
-    #for index, elem in enumerate(seq[0]):    # Creating single array with all sequence wavefroms concatenated
-     #   CompleteSeqWav = np.concatenate((CompleteSeqWav,np.array(elem.waveform)))
-        
-    #CompleteSeqWavInv = INV.InverseHPfilterSeq(CompleteSeqWav, R = 2E7, C = 1E-9)   # Making HP inverse on concatenated wavefroms
-    # 
-    # 
-    #print(len(CompleteSeqWav), len(CompleteSeqWavInv))
-    #
-    #stop = 0 
-    #for index, elem in enumerate(seq[0]):  # Slicing HP inverted concatenated wavefroms back into sequnce elements
-    #    start = stop 
-    #    stop = start + len(elem.waveform)
-    #    elem.waveform = CompleteSeqWavInv[start:stop]
-    
-    #plt.figure("WAVEFORM_INVERTED")
-    #plt.plot(seq[0][2].waveform)
-    #plt.show()
-    
-    #print("Lch1",len(seq[0][2].waveform),"Lch2",len(seq[1][2].waveform))
-
-    #seq = filter(None, seq)  # Remove all empty elements from list
-
     # Calculate average of whole sequence and sequence length
     aver_list = list()  # List for storing average value of whole sequence for all channels
-    sum_list = list()  # List for storing area or integral of whole sequence for all channels
     weigths = list()  # List containing timings of all elements in the sequence - needed for calculation of weigthed average
     time_seq =  0 # Sequence time
     for ch_num in xrange(len(seq)):
@@ -77,21 +68,19 @@ def set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync = None, compensate = 
                 weigths.append(sum(seq_elem.timings.values()))   # Storing complete time of single element in sequence 
             aver_list[ch_num].append(seq_elem.waveform.mean())  # Adding average for every element of one channel in the sequence
         aver_list[ch_num] = np.average(aver_list[ch_num], weights=weigths)
-        #aver_list[ch_num] = np.mean(aver_list[ch_num]) # Calculating average for complete sequence for every channel
+        
 
     print ("aver_list",aver_list)
     print ("time_seq",time_seq)
 
-    #for ch_num in xrange(len(seq)):
-        #sum_list.append(aver_list[ch_num]*time_seq)   # Calculating area or integral of whole sequence for all channels
-
-    
+   
     # Compensation element calculation principle:
     # First calculate compensation element time based on sequence area and AWGMax_amp using formula: time_comp = area/AWGMax_amp , for all channels
     # Find maximum compensation element time of all channels and set compensation element time time_comp to that one
-    # For each channel calculate amplitude of compensation element amp_comp based on time_comp 
+    # For each channel calculate amplitude of compensation element amp_comp based on time_comp, formula: amp_comp = (mean value*overall sequence time)/time_comp - line 103
     # Reason for this approach is AWG requirement that all channels need to have same number of points - compensation element time need to be the same for all channels
 
+    # Calculating compensation element time
     time_comp = 0 # Time of compensation element
     for ch_num in xrange(len(seq)):
         AWGMax_amp_real_units = AWGMax_amp/(seq[ch_num][0].AmpUnitsDict[seq[ch_num][0].AmpUnitsKey])  # Converting AWGMax_amp to real units for sequence for each channel
@@ -100,18 +89,14 @@ def set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync = None, compensate = 
             time_comp = time_comp_temp
 
     time_comp = time_comp*5 # Increasing time of compensation element to achieve lower compensation amplitude 
-    print ("time_comp",time_comp)
-
-
-    
-    
+    print ("time_comp",time_comp) 
     
 
     # Creating two first elements in sequence - sync and compensate
     for ch in xrange(len(seq)):
         
          
-        amp_comp = -(aver_list[ch]*time_seq)/time_comp # If mean value of sequence is positive, amplitude of compensation pulse need to be negative and vice versa
+        amp_comp = -(aver_list[ch]*time_seq)/time_comp # If mean value of sequence is positive, amplitude of compensation pulse needs to be negative and vice versa
 
 
         print ("amp_comp", amp_comp)
@@ -152,26 +137,25 @@ def set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync = None, compensate = 
     for ch in xrange(len(seq)):
 
         if 'CH1' in seq[ch][0].waveform_name:
-            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in list below
+            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in the list below
                 seq[ch] = [sync.CH1] + seq[ch]
             else:               # If time of compensation element is nonzero -> compensation element is needed
                 seq[ch] = [sync.CH1, compensate.CH1] + seq[ch]
 
         elif 'CH2' in seq[ch][0].waveform_name:
-            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in list below
+            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in the list below
                 seq[ch] = [sync.CH2] + seq[ch]
             else:               # If time of compensation element is nonzero -> compensation element is needed
                 seq[ch] = [sync.CH2, compensate.CH2] + seq[ch]
             
         elif 'CH3' in seq[ch][0].waveform_name:
-            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in list below
+            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in the list below
                 seq[ch] = [sync.CH3] + seq[ch]
             else:               # If time of compensation element is nonzero -> compensation element is needed
                 seq[ch] = [sync.CH3, compensate.CH3] + seq[ch]
              
-
         elif 'CH4' in seq[ch][0].waveform_name:
-            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in list below
+            if time_comp == 0:   # If time of compensation element is zero -> mean value is zero -> no need for compensation element -> not putting it in the list below
                 seq[ch] = [sync.CH4] + seq[ch]
             else:               # If time of compensation element is nonzero -> compensation element is needed
                 seq[ch] = [sync.CH4, compensate.CH4] + seq[ch]
@@ -231,10 +215,7 @@ def set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync = None, compensate = 
 
             
             
-
-                
-               
-                           
+              
     #Turn on the AWG channels
     #AWG._ins.do_set_output(1,1)
     #AWG._ins.do_set_output(1,2)
--- MeasureScripts/AWG_upload.py
+++ MeasureScripts/AWG_upload.py
@@ -13,7 +13,7 @@ AWG_clock = 10e6        # Wanted AWG clock. Info https://www.google.at/url?sa=t&
 											# In pdf on link read section "AWG: Simple Concept, Maximum Flexibility"
 											
 						# Take care about waveform and sequence length and clock rate  - AWG has limited capability
-AWGMax_amp = 2          # In AmpUnits!!! Maximum needed amplitude on all channels for your particular experiment (noise reduction) - need to be set at the beginning
+AWGMax_amp = 2          # In Volts!!! Maximum needed amplitude on all channels for your particular experiment (noise reduction) - need to be set at the beginning
 Seq_length = 15       # Sequence length (number of periods - waveforms)
 t_sync = 2              # Duration of synchronization element in sequence in "TimeUnits"
 Automatic_sequence_generation = False   # Flag for determining type of sequence generation: Automatic - True,  Manual - False 
@@ -47,7 +47,7 @@ if Automatic_sequence_generation:   # If user wants Automatic sequence generatio
     seq2 = seq
 
 
-    AWG_lib.set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync, compensate, t_sync)  # Function for uploading and setting all sequence waveforms to AWG 
+    AWG_lib.set_waveform(seq,AWG_clock,AWGMax_amp, sync, compensate, t_sync)  # Function for uploading and setting all sequence waveforms to AWG 
 
 
 
@@ -71,13 +71,12 @@ if not(Automatic_sequence_generation):  # If user wants manual sequence generati
                                                                                                                          # because sync and compensate sequence elements are 1st and 2nd
         
         if i == 0:
-            
             p.setValuesCH1([2,0], [2,0], [2,0])  # Starting element in sequence with zero amp for synchronization reasons
             p.setMarkersCH1([0,0,0],[0,0,0])   # Starting element in sequence with zero marker amp for synchronization reasons
         else:
             p.setValuesCH1([0.5,A1[0]], [0.5,A1[1]], [0.5,A1[2]]) # Setting waveform shape for one wavefrom object p in sequence seq for AWG channel 1 - [Time1,Amp1],[Time2,Amp2]...  Time in TimeUnits and Amp in AmpUnits
             p.setMarkersCH1([1,1,1],[0,0,0])  # Setting marker just in the first wavefrom of the sequence (further is zero)
-        #A1[1] = A1[1] - delta_A1 # Defining amplitude change between wavefroms in sequence
+            A1[1] = A1[1] - delta_A1 # Defining amplitude change between wavefroms in sequence
 
         
         if i == 0:
@@ -87,7 +86,7 @@ if not(Automatic_sequence_generation):  # If user wants manual sequence generati
         else:
             p.setValuesCH2([0.5,A2[0]], [0.5,A2[1]], [0.5,A2[2]]) # Setting waveform shape for one wavefrom object p in sequence seq for AWG channel 1 - [Time1,Amp1],[Time2,Amp2]...  Time in TimeUnits and Amp in AmpUnits
             p.setMarkersCH2([1,1,1],[0,0,0])  # Setting marker just in the first wavefrom of the sequence (further is zero)
-        #A2 = A2 - delta_A2 # Defining amplitude change between wavefroms in sequence
+            A2[1] = A2[1] - delta_A2 # Defining amplitude change between wavefroms in sequence
     
 
     
@@ -100,6 +99,6 @@ if not(Automatic_sequence_generation):  # If user wants manual sequence generati
 
 
 
-    AWG_lib.set_waveform(seq,AWG_clock,AWGMax_amp,Seq_length, sync, compensate, t_sync) # Function for uploading and setting all sequence waveforms to AWG 
+    AWG_lib.set_waveform(seq,AWG_clock,AWGMax_amp, sync, compensate, t_sync) # Function for uploading and setting all sequence waveforms to AWG 
     
     raw_input("Press Enter if uploading to AWG is finished")
